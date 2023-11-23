e2338663eb6eabb07879b786f0aacf38359afbb3
Khateeb_Rehman <112754572+Khateeb1709@users.noreply.github.com>
2023-11-23 23:52:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,42 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let obj = {};
+	
+ // 4 3 2 6
+  arr.forEach((element) => {
+	  if(obj["element"] === undefined){
+		  obj["element"] = 1;
+	  }
+	  else{
+		  obj["element"] += 1;
+	  }
+  });
+
+  let n = arr.length;
+  let minCost = 0;
+
+  while(n >= 2){
+      let [f, s] = Object.keys(x);
+	  
+      // [4, 3, 2, 6]
+	  
+	  /* {
+	     2 : 0,
+	     3 : 0,
+	     4 : 1,
+	     6 : 1
+	*/ 
+	  
+	  minCost = minCost + f + s;
+
+	  obj["f"]--;
+	  obj["s"]--;
+
+	  n--;
+  }
+	
+  return minCost;
 }
 
-module.exports=mincost;
+module.exports=mincost; 
