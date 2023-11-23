7f4de254aac7d42821200fa84484dffd8e40066d
Khateeb_Rehman <112754572+Khateeb1709@users.noreply.github.com>
2023-11-24 01:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,64 +1,20 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  let obj = {};
-	
- // 4 3 2 6
-  for(let i=0;i<arr.length;i++){
-	  let ele = arr[i];
-	  
-	  if(obj.ele === undefined){
-		  obj.ele = 1
-	  }
-	  else{
-		 obj.ele += 1;	  
-	}
+  let minimumCost = 0;
+
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+
+    const smallestA = arr.shift();
+    const smallestB = arr.shift();
+
+    const length = smallestA + smallestB;
+    minimumCost += length;
+
+    arr.push(length);
   }
 
-  let n = arr.length;
-  let minCost = 0;
-
-  while(n >= 2){
-      let arr = Object.keys(obj);
-	  
-      // [4, 3, 2, 6]
-	  
-	  /* {
-	     2 : 0,
-	     3 : 0,
-	     4 : 1,
-	     6 : 1
-	*/ 
-      let f = 0;
-	  let s = 0;
-	  
-	  if(obj[arr[0]] > 1){
-		  f = arr[0];
-		  s = arr[0];
-	  }
-	  else{
-		  f = arr[0];
-		  s = arr[1];
-	  }
-	  
-	  minCost = minCost + f + s;
-
-	  obj.f--;
-	  if(obj.f === 0 )delete obj.f;
-	  obj.s--;
-      if(obj.s === 0 )delete obj.s;
-
-	  if(obj.minCost === undefined){
-         obj.minCost = 1;
-	  }
-	  else{
-		  obj.minCost += 1;
-	  }
-	  n--;
-  }
-	
-  return minCost;
+  return minimumCost;
 }
 
 module.exports=mincost; 
